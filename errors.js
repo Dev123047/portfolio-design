cccd8ee3be81bf63fabb8ade83c331c14204e7d8
Bhavy Garg <66347950+Dev123047@users.noreply.github.com>
2024-12-19 17:56:16 +0530

    Add files via upload

---
+++ errors.js
@@ -0,0 +1,9 @@
+const user_not_found = "0E0 : User not found!";
+const login_aborted = "0E1 : Login aborted!";
+const password_incorret = "0E2 : Password incorrect!"
+const not_authorized = "0E3 : Forbidden area! Typically due to restrictions on your current user."
+const file_not_found = "0E4 : The requested file was not found in the current directory."
+const dir_not_found = "0E5 : The requested directory/subdirectory was not found."
+const dir_exists = "0E6 : The directory already exists."
+const file_exists = "0E7 : THe file already exists!"
+const host_not_found = "0E8 : The host was not found!"
